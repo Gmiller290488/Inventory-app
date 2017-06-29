3726b8a9fccd578d8b958cd446e43cb29a49aeb2
Gareth Miller <gmiller290488@gmail.com>
2017-06-29 21:21:40 +0100

    Implemented insertProduct method

--- app/src/main/java/com/ctingcter/inventory/EditorActivity.java
+++ app/src/main/java/com/ctingcter/inventory/EditorActivity.java
@@ -43,7 +43,6 @@ public class EditorActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_add_project);
 
 
-
         // Examine the intent that launched the activity
         // Figure if it's editing product or adding a new one
         Intent intent = getIntent();
@@ -58,8 +57,47 @@ public class EditorActivity extends AppCompatActivity implements
         } else {
             setTitle(getString(R.string.editor_activity_title_edit_product));
         }
+    }
+
+
+    private void insertProduct() {
+        String titleString = mProductEditText.getText().toString().trim();
+        String quantityString = mQuantityeditText.getText().toString().trim();
+        int quantity = Integer.parseInt(quantityString);
+        String priceString = mPriceEditText.getText().toString().trim();
+        int price = Integer.parseInt(priceString);
+
+        // Create Database helper
+        ProductDbHelper mDbHelper = new ProductDbHelper(this);
+
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and pet attributes from the editor are the values.
+        ContentValues values = new ContentValues();
+        values.put(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME, titleString);
+        values.put(ProductContract.ProductEntry.COLUMN_QUANTITY, quantity);
+        values.put(ProductContract.ProductEntry.COLUMN_PRICE, price);
+        //values.put(PetEntry.COLUMN_PET_BREED, breedString);
+        //values.put(PetEntry.COLUMN_PET_GENDER, mGender);
+        //values.put(PetEntry.COLUMN_PET_WEIGHT, weight);
+
+        // Insert a new row for pet in the database, returning the ID of that new row.
+        long newRowId = db.insert(ProductContract.ProductEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving pet", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Pet saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
 
     }
+
+
 
 
     @Override
