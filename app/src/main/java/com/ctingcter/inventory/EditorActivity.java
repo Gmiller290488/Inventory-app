29ef3d72ea687ae32b819e5db9fc542273a4907c
Gareth Miller <gmiller290488@gmail.com>
2017-07-01 10:30:37 +0100

    Added save button functionality and error checks

--- app/src/main/java/com/ctingcter/inventory/EditorActivity.java
+++ app/src/main/java/com/ctingcter/inventory/EditorActivity.java
@@ -25,7 +25,7 @@ import com.ctingcter.inventory.data.ProductDbHelper;
  */
 
 public class EditorActivity extends AppCompatActivity implements
-        LoaderManager.LoaderCallbacks<Cursor> {
+        LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener {
 
     private static final int EXISTING_PRODUCT_LOADER = 0;
 
@@ -42,7 +42,14 @@ public class EditorActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_project);
 
+        // All of the declarations for the EditTexts
+        mProductEditText = (EditText) findViewById(R.id.id_product_EV);
+        mPriceEditText = (EditText) findViewById(R.id.id_price_ET);
+        mQuantityeditText = (EditText) findViewById(R.id.id_quantity_ET);
+        mSupplierEditText = (EditText) findViewById(R.id.id_supplier_ET);
 
+        Button save = (Button) findViewById(R.id.id_save_btn);
+        save.setOnClickListener(this);
         // Examine the intent that launched the activity
         // Figure if it's editing product or adding a new one
         Intent intent = getIntent();
@@ -62,9 +69,25 @@ public class EditorActivity extends AppCompatActivity implements
 
     private void insertProduct() {
         String titleString = mProductEditText.getText().toString().trim();
+        if (titleString.matches("")){
+            Toast.makeText(this, "Please enter the product name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String supplierString = mSupplierEditText.getText().toString().trim();
+        if (supplierString.matches("")){
+            Toast.makeText(this, "Please enter the supplier name", Toast.LENGTH_SHORT).show();
+            return;
+        }
         String quantityString = mQuantityeditText.getText().toString().trim();
+        if (quantityString.matches("")){
+            quantityString = "0";
+        }
         int quantity = Integer.parseInt(quantityString);
         String priceString = mPriceEditText.getText().toString().trim();
+        if (titleString.matches("")){
+            Toast.makeText(this, "Please enter the price", Toast.LENGTH_SHORT).show();
+            return;
+        }
         int price = Integer.parseInt(priceString);
 
         // Create Database helper
@@ -73,27 +96,25 @@ public class EditorActivity extends AppCompatActivity implements
         // Gets the database in write mode
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
-        // Create a ContentValues object where column names are the keys,
+         //Create a ContentValues object where column names are the keys,
         // and pet attributes from the editor are the values.
         ContentValues values = new ContentValues();
         values.put(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME, titleString);
         values.put(ProductContract.ProductEntry.COLUMN_QUANTITY, quantity);
         values.put(ProductContract.ProductEntry.COLUMN_PRICE, price);
-        //values.put(PetEntry.COLUMN_PET_BREED, breedString);
-        //values.put(PetEntry.COLUMN_PET_GENDER, mGender);
-        //values.put(PetEntry.COLUMN_PET_WEIGHT, weight);
+        values.put(ProductContract.ProductEntry.COLUMN_SUPPLIER, supplierString);
 
-        // Insert a new row for pet in the database, returning the ID of that new row.
+       //  Insert a new row for pet in the database, returning the ID of that new row.
         long newRowId = db.insert(ProductContract.ProductEntry.TABLE_NAME, null, values);
 
-        // Show a toast message depending on whether or not the insertion was successful
+       //  Show a toast message depending on whether or not the insertion was successful
         if (newRowId == -1) {
-            // If the row ID is -1, then there was an error with insertion.
-            Toast.makeText(this, "Error with saving pet", Toast.LENGTH_SHORT).show();
+         //    If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error with saving product", Toast.LENGTH_SHORT).show();
         } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
-            Toast.makeText(this, "Pet saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
-        }
+       //      Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Product saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+       }
 
     }
 
@@ -113,5 +134,14 @@ public class EditorActivity extends AppCompatActivity implements
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.id_save_btn:
+                //Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+                insertProduct();
+        }
     }
 }
