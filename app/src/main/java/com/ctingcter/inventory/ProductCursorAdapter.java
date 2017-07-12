092f1a5d7a332e13e45a926614397df989c347f0
Gareth Miller <gmiller290488@gmail.com>
2017-07-12 18:41:26 +0100

    Sorted sell button

--- app/src/main/java/com/ctingcter/inventory/ProductCursorAdapter.java
+++ app/src/main/java/com/ctingcter/inventory/ProductCursorAdapter.java
@@ -1,14 +1,22 @@
 package com.ctingcter.inventory;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ctingcter.inventory.data.ProductContract;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 /**
  * Created by CTingCTer on 30/04/2017.
@@ -19,7 +27,8 @@ public class ProductCursorAdapter extends CursorAdapter {
     public ProductCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
     }
-
+        private int quantity;
+        private int rowId;
     /**
      * Makes a new blank list item view. No data is set (or bound) to the views yet.
      *
@@ -47,10 +56,10 @@ public class ProductCursorAdapter extends CursorAdapter {
      *                correct row.
      */
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
         // Find individual views that we want to modify in the list item layout
         TextView productTextView = (TextView) view.findViewById(R.id.id_product_TV);
-        TextView quantityTextView = (TextView) view.findViewById(R.id.id_quantity_TV);
+        final TextView quantityTextView = (TextView) view.findViewById(R.id.id_quantity_TV);
         TextView priceTextView = (TextView) view.findViewById(R.id.id_price_TV);
 
 
@@ -58,16 +67,45 @@ public class ProductCursorAdapter extends CursorAdapter {
         int productColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
         int quantityColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_QUANTITY);
         int priceColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRICE);
+        int rowColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry._ID);
 
         // Read the pet attributes from the Cursor for the current pet
         String productName = cursor.getString(productColumnIndex);
-        String productQuantity = cursor.getString(quantityColumnIndex);
+        final String productQuantity = cursor.getString(quantityColumnIndex);
         String price = cursor.getString(priceColumnIndex);
 
+        rowId = cursor.getInt(rowColumnIndex);
+        quantity = cursor.getInt(quantityColumnIndex);
         // Update the TextViews with the attributes for the current pet
         productTextView.setText(productName);
         quantityTextView.setText(productQuantity);
         priceTextView.setText(price);
 
+        Button sell = (Button) view.findViewById(R.id.id_sell_Btn);
+        sell.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (quantity == 0) {
+                    Toast toast = Toast.makeText(context, "There are no more of this item", Toast.LENGTH_SHORT);
+                    toast.show();
+
+                    return;
+
+                } else {
+                    quantity--;
+                    ContentValues values = new ContentValues();
+                    values.put(ProductContract.ProductEntry.COLUMN_QUANTITY, quantity);
+                    Uri newUri = ContentUris.withAppendedId(ProductContract.ProductEntry.CONTENT_URI, rowId);
+                    int rowsUpdated = context.getContentResolver().update(newUri, values, null, null);
+                    quantityTextView.setText(String.valueOf(quantity));
+
+
+                }
+            }
+        });
+
     }
+
+
 }
